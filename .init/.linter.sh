bc738f2c17473050e6221a082ea9919480d1a5bf
Kavia AI <production@kavia.ai>
2025-10-30 03:14:14 +0000

    CheckPoint - cg30483761

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/booknest-mobile-app-40323-40332/booknest_mobile_frontend
+flutter analyze
+
---
+++ booknest_mobile_frontend/android/app/src/main/kotlin/com/example/booknest_mobile_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.booknest_mobile_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
